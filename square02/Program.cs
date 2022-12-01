b46e9e9d0fb5880aabc3b793aaa253fd3a872f50
Olga <olga@podolyanchenko.ru>
2022-12-02 04:40:07 +0700

    1е квадрат 2го

---
+++ square02/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число:");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int B = Convert.ToInt32(Console.ReadLine());
+if (B * B == A)
+ Console.WriteLine("yes");
+else
+ Console.WriteLine("no");
+
