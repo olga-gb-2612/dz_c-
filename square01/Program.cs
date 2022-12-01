654780402efe8dcc3a1ba1ef6f45dc76943039f7
Olga <olga@podolyanchenko.ru>
2022-12-02 03:56:05 +0700

    Возведение в квадрат

---
+++ square01/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+
+Console.Write("Введите число для возведения в квадрат:");
+int A = Convert.ToInt32(Console.ReadLine());
+int Res = A * A;
+Console.Write("Результат:");
+Console.Write(Res);
