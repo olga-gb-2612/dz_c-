563033d6b248e932fe249dd3162d7327e184ee7f
Olga <olga@podolyanchenko.ru>
2022-12-02 04:18:51 +0700

    интерполяция

--- square01/Program.cs
+++ square01/Program.cs
@@ -3,5 +3,6 @@
 Console.Write("Введите число для возведения в квадрат:");
 int A = Convert.ToInt32(Console.ReadLine());
 int Res = A * A;
-Console.Write("Результат:");
+Console.Write("Результат:");    
 Console.Write(Res);
+//Console.WriteLine($"Результат = {A * A}"); интерполяция
