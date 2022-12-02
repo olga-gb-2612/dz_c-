986e93dc76f705932a8a6ad5dcad1760a04e39a3
Olga <olga@podolyanchenko.ru>
2022-12-03 02:30:33 +0700

    изменила структуру в проводнике

--- square01/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.Clear();
-
-Console.Write("Введите число для возведения в квадрат:");
-int A = Convert.ToInt32(Console.ReadLine());
-int Res = A * A;
-Console.Write("Результат:");    
-Console.Write(Res);
-//Console.WriteLine($"Результат = {A * A}"); интерполяция
