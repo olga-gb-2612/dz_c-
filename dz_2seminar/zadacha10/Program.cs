03ed711e525924bd21eeb45c76086013d1fa3e73
Olga <olga@podolyanchenko.ru>
2022-12-03 03:07:52 +0700

    Задание 10

---
+++ dz_2seminar/zadacha10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите трехзначное число:");
+int A = Convert.ToInt32(Console.ReadLine()); 
+if (A > 99 && A < 1000)
+ Console.Write((A % 100)/10);
+else
+ Console.Write("Ошибка!");
+
+
+
