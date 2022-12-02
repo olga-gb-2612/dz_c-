bab96f0ab1ada9a436c0dd05742d9ba177470c43
Olga <olga@podolyanchenko.ru>
2022-12-03 04:35:33 +0700

    Задание 15

---
+++ dz_2seminar/zadacha15/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число, обозначающее день недели:");
+int A = Convert.ToInt32(Console.ReadLine()); 
+if (A < 1 || A > 7)
+   Console.Write("Ошибка!");
+else if ( A == 6 || A == 7)
+   Console.Write("да");
+else
+   Console.Write("нет");
