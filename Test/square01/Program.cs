83aa7e23fa743c67e9e33c4da4a083cc3f41a536
Olga <olga@podolyanchenko.ru>
2022-12-03 02:32:02 +0700

    добавила папку

---
+++ Test/square01/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+
+Console.Write("Введите число для возведения в квадрат:");
+int A = Convert.ToInt32(Console.ReadLine());
+int Res = A * A;
+Console.Write("Результат:");    
+Console.Write(Res);
+//Console.WriteLine($"Результат = {A * A}"); интерполяция
