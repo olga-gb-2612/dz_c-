a58ae6df1867075d5dce8eb474490c17e3aed0a0
Olga <olga@podolyanchenko.ru>
2022-12-03 02:17:19 +0700

    Задание 6

---
+++ dz_1seminar/zadacha6/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число для проверки на четность:");
+int A = Convert.ToInt32(Console.ReadLine());
+if ( A % 2 == 0)
+ Console.WriteLine("да");
+else
+ Console.WriteLine("нет");
