800cab0353e71f8d05a326c9a637ae7e4f8310b9
Olga <olga@podolyanchenko.ru>
2022-12-03 01:28:13 +0700

    Приступаю к выполнению дз

---
+++ dz_1seminar/zadacha2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите первое число:");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int B = Convert.ToInt32(Console.ReadLine());
+if (A > B)
+Console.WriteLine($"MAX = {A}, MIN = {B}");
+else
+Console.WriteLine($"MAX = {B}, MIN = {A}");
