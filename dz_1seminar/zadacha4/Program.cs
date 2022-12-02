4d4bb491fe208e8fc71b5c03640117a45591972c
Olga <olga@podolyanchenko.ru>
2022-12-03 02:09:58 +0700

    Задание 4

---
+++ dz_1seminar/zadacha4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число:");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число:");
+int C = Convert.ToInt32(Console.ReadLine());
+if( A >= B && A >= C )
+     Console.WriteLine($"MAX = {A}");
+else if (B >= A && B >= C)
+     Console.WriteLine($"MAX = {B}");
+else 
+     Console.WriteLine($"MAX = {C}");
