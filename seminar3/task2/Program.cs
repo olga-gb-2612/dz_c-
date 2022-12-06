98054b718c7be16f393e6f3b6f9576d43cfff6e7
Olga <olga@podolyanchenko.ru>
2022-12-07 00:17:25 +0700

    обратная задача

---
+++ seminar3/task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите номер четверти:");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x == 1 || x == 2)
+  Console.WriteLine("y > 0");
+else
+  Console.WriteLine("y < 0");
+
+if (x == 1 || x == 4)
+  Console.WriteLine("x > 0");
+else
+  Console.WriteLine("x < 0");
