ac6eb0493ad1c42950a365e8b2e26864ae4a3f5b
Olga <olga@podolyanchenko.ru>
2022-12-07 00:58:56 +0700

    квадрат всех чисел до n

---
+++ seminar3/task4/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("Введите число:");
+int x = Convert.ToInt32(Console.ReadLine());
+for (int i =1; i <=x; i++)
+  Console.Write($"{i*i} "); // можно через Math.Pow(i, 2)
