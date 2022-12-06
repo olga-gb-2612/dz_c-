6adf37c1ebf673b32a331464f2f99d8002090aa5
Olga <olga@podolyanchenko.ru>
2022-12-07 01:50:23 +0700

    палиндром

---
+++ dz_3seminar/task19/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число:");
+string? x = Console.ReadLine();
+
+while ( x.Length > 5 || x.Length < 5 )
+{
+    Console.Write("Вы ошиблись!\nВведите пятизначное число:");
+    x = Console.ReadLine();
+}
+if (x[0]==x[4] && x[1]==x[3])
+  Console.WriteLine("да");
+else
+  Console.WriteLine("нет");
