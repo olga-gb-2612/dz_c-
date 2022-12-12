2fc5eb3da63036b1ab2e3ad27e4a76db267c03cc
Olga <olga@podolyanchenko.ru>
2022-12-12 23:01:42 +0700

    семинар 4

---
+++ seminar4/array/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите кол-во жлементов массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for(int i = 0; i < array.Length; i++)
+     array[i] = new Random().Next(1, 11);
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
