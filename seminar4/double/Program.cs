d0d17111ea0fb627f4433b46f01d6d69fcedace2
Olga <olga@podolyanchenko.ru>
2022-12-13 00:22:15 +0700

    генерация дробных чисел

---
+++ seminar4/double/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите кол-во жлементов массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+for(int i = 0; i < array.Length; i++)
+ {                                              // * (end-begin)+begin
+     array[i] = Math.Round(new Random().NextDouble() * (50 -10) + 10, 2); //[0,1]
+ }
+Console.WriteLine($"[{string.Join(", ", array)}]");
