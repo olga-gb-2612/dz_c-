2acf2d23e3c185a7f3068bb58a246d21ca9e1130
Olga <olga@podolyanchenko.ru>
2022-12-13 09:44:08 +0700

    Задача 25

---
+++ dz_4seminar/task25/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень:");
+int b= Convert.ToInt32(Console.ReadLine());
+int res = 1;
+for(int i=1; i<=b;i++)
+res=res*a;
+Console.WriteLine (res);
