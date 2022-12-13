91677817a71d4b65eea70428231389805c6c57b3
Olga <olga@podolyanchenko.ru>
2022-12-13 10:24:23 +0700

    Задача 27

---
+++ dz_4seminar/task27/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число, где цифры записываются через пробел:");
+string[] a = Console.ReadLine().Split(' ');
+int sum =0;
+for (int i = 0; i < a.Length; i++)
+    sum += int.Parse(a[i]);
+Console.Write(sum);
+
+
