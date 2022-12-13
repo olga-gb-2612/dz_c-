4d8ef53e5bbf6f69e2c0342fc7011ad2609dc3cd
Olga <olga@podolyanchenko.ru>
2022-12-14 00:15:56 +0700

    кол=во элементов от 10 до 99

---
+++ seminar5/task35/Program.cs
@@ -0,0 +1,25 @@
+﻿// выводит кол-во элементом от 10 до 100
+
+void inputArray(int[] array)
+{
+    for(int i=0; i<array.Length; i++)
+     array[i] = new Random().Next(-100,101); 
+}
+
+int ReleaseArray(int[] array)
+{
+int count = 0;
+for (int i = 0; i <array.Length; i++)
+{
+ if (array[i] > 9 && array[i] < 100)
+ count++;
+}
+return count;
+}
+
+
+Console.Clear();
+int[] array = new int[123];
+inputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine(ReleaseArray(array));
