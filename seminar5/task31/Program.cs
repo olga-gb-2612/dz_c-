a6b6af9e1f8b546af31f224e96dd176f26631605
Olga <olga@podolyanchenko.ru>
2022-12-13 21:52:44 +0700

    функции на заполнение массива и поиск мин и макс

---
+++ seminar5/task31/Program.cs
@@ -0,0 +1,31 @@
+﻿// массив из 12 элементов, заполненный случайныйми числами
+//из промежутка[-9;9]. найдите сумму отрицат и положит. элементов массива.
+
+void inputArray(int[] array)
+{
+    for(int i=0; i<array.Length; i++)
+     array[i] = new Random().Next(-9,10); 
+}
+
+void ReleaseArray(int[] array)
+{
+   int sumPositive = 0, sumNegative = 0;
+   for (int i = 0; i <array.Length; i++)
+   {
+    if (array[i] > 0)
+       sumPositive += array[i];
+    else
+       sumNegative += array[i];   
+   }
+   Console.WriteLine($"Сумма положительных чисел равна {sumPositive}");
+   Console.WriteLine($"Сумма отрицательных чисел равна {sumNegative}");
+
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+inputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+ReleaseArray(array);
