29f371cdb63542b5e76aa0d276c077fa5cf28bd9
Olga <olga@podolyanchenko.ru>
2022-12-13 09:54:43 +0700

    Задача 29

---
+++ dz_4seminar/task29/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите кол-во элементов массива:");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[a];
+for(int i = 0; i < a; i++)
+{
+Console.Write($"Введите {i}-ый элемент массива:");
+array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"[{string.Join(", ", array)}]");
