6233c24cf2d190157c46d256bb9c07d2fc48a421
Olga <olga@podolyanchenko.ru>
2022-12-13 07:45:45 +0700

    функция на поиск большего

---
+++ seminar4/function/Program.cs
@@ -0,0 +1,13 @@
+﻿int test (int a, int b) //string / b=10
+{
+     if (a > b) 
+     return a; //">"
+     return b; //"<"
+}
+
+Console.Clear();
+Console.Write("Введите 1-е число:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-е число:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(test(n, m));
