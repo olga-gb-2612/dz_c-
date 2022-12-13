45f72d25dcb034381619f63298e455ce996b30df
Olga <olga@podolyanchenko.ru>
2022-12-13 07:53:24 +0700

    процедура

---
+++ seminar4/void/Program.cs
@@ -0,0 +1,12 @@
+﻿void test (ref int a) // ref передача по ссылке, с массивами не работает.
+{
+    a += 5;
+}
+
+Console.Clear();
+Console.Write("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+test(ref n);
+Console.WriteLine(n);
+
+//процедуры для повторения участка кода
