e4fde2ed470b491eabbbdab2bf62595de0617652
Olga <olga@podolyanchenko.ru>
2022-12-13 22:38:51 +0700

    задача 32

---
+++ seminar5/task32/Program.cs
@@ -0,0 +1,23 @@
+﻿// замена эл-ов массива: положительные замените на отрицательные, и наоборот.
+
+void inputArray(int[] array)
+{
+    for(int i=0; i<array.Length; i++)
+     array[i] = new Random().Next(-9,10); 
+}
+
+
+void OtriPol(int[] array)
+{
+    for(int i = 0; i <array.Length; i++)
+       array[i]=array[i]*(-1);  
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+inputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+OtriPol(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
