c7e6191f673a223418e4f0bb830d3fe90f10905b
Olga <olga@podolyanchenko.ru>
2022-12-14 21:15:44 +0700

    решение дз

---
+++ dz_seminar5/task34/Program.cs
@@ -0,0 +1,29 @@
+﻿//показывает кол-во четных чисел в массиве.
+
+void inputArray(int[] array)
+{
+    for(int i=0; i<array.Length; i++)
+     array[i] = new Random().Next(100,1000); 
+}
+
+
+void ReleaseArray(int[] array)
+{
+   int count=0;
+   for (int i = 0; i <array.Length; i++)
+   {
+    if (array[i] %2 == 0)
+       count++ ;
+   }
+   Console.WriteLine($"Кол-во четных чисел в массиве : {count}");
+  
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+inputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+ReleaseArray(array);
+
