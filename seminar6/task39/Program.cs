39242318a781d98af1e86950399740a111166325
Olga <olga@podolyanchenko.ru>
2022-12-17 00:32:20 +0700

    Задача 39

---
+++ seminar6/task39/Program.cs
@@ -0,0 +1,26 @@
+﻿// перевернуть массив
+
+void InputArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+       array[i] = new Random().Next(1,11);
+}
+
+void ReleaseArray(int[] array)
+{
+    for(int i = 0; i < array.Length / 2; i++)
+    {
+        int t = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = t;
+    }
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+ReleaseArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
