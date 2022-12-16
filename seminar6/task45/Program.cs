947798708ff4e2e2602d6c2559baeaba222bc308
Olga <olga@podolyanchenko.ru>
2022-12-17 01:52:29 +0700

    поэлементне копирование

---
+++ seminar6/task45/Program.cs
@@ -0,0 +1,10 @@
+﻿// поэлементное копирование
+
+int[] a = {1, 2, 3, 4, 5};
+int[] b = new int[a.Length];
+for (int i = 0; i < a.Length; i++)
+b[i] = a[i];
+
+b[0] = -1;
+Console.WriteLine(string.Join(" ", a));
+Console.WriteLine(string.Join(" ", b));
