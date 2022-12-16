5a5e0a6b5886ffe1e11ac13a1f5509f262540237
Olga <olga@podolyanchenko.ru>
2022-12-17 00:59:28 +0700

    10->2

---
+++ seminar6/task42/Program.cs
@@ -0,0 +1,14 @@
+﻿// перевод из десятичной в 2ую систему исчисления
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string result = string.Empty;
+while (n > 0)
+{
+result = Convert.ToString(n % 2) + result;
+n /= 2; // n = n / 2;
+}
+Console.WriteLine(result);
+
+
