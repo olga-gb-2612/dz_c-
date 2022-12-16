ee018b5357725b19c895615b8ea506abc69c0cee
Olga <olga@podolyanchenko.ru>
2022-12-17 01:12:11 +0700

    фибоначчи

---
+++ seminar6/task44/Program.cs
@@ -0,0 +1,12 @@
+﻿// фибоначчи
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), a0 = 0, a1 = 1;
+for (int i = 0; i < n; i++)
+{
+Console.Write($"{a0} ");
+int x = a0 + a1;
+a0 = a1;
+a1 = x;
+}
