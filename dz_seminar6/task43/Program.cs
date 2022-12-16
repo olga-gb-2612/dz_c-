b5a6ee99e7c0063608e9fb31e69ea70d03db785e
Olga <olga@podolyanchenko.ru>
2022-12-17 02:30:59 +0700

    точка пересечения

---
+++ dz_seminar6/task43/Program.cs
@@ -0,0 +1,15 @@
+﻿// найти точку пересечения
+
+
+Console.Clear();
+Console.WriteLine("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+double x = (b2-b1)/(k1-k2);
+double y = k1*x + b1;
+Console.WriteLine($"Точка пересечения: ({x};{y})");
