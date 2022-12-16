39fcea968b9dbc963fb7d7bbe41140ab9980f0c7
Olga <olga@podolyanchenko.ru>
2022-12-17 00:45:17 +0700

    Треугольник

---
+++ seminar6/task41/Program.cs
@@ -0,0 +1,11 @@
+﻿// провекра может ли треугольник получиться из введенных сторон
+
+Console.Clear();
+Console.Write("Введите 3 числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+if (a < b + c && b < a + c && c < a + b)
+Console.WriteLine("Треугольник");
+else
+Console.WriteLine("Нет");
