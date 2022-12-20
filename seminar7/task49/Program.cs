849b80ec0c75bfd9375b2296e31347a2ffb260c3
Olga <olga@podolyanchenko.ru>
2022-12-21 01:35:09 +0700

    'элемент в квадрате на нечетных индексах

---
+++ seminar7/task49/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте двумерный массив. Найдите элементы, у которых оба индекса нечетные, и замените эти элементы на их квадраты.
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 11); // [-10, 10]
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void ReleaseMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        if (i % 2 == 1)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (j % 2 == 1)
+                    matrix[i, j] *= matrix[i, j];
+            }
+        }
+    }
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }       
+}
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine();
+ReleaseMatrix(matrix);
