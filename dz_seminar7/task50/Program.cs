ac8f7c840750767e08449d65b20b1b9b511c0566
Olga <olga@podolyanchenko.ru>
2022-12-21 04:21:59 +0700

    поправила программу

--- dz_seminar7/task50/Program.cs
+++ dz_seminar7/task50/Program.cs
@@ -14,6 +14,16 @@ void InputMatrix(int[,] matrix)
     }
 }
 
+void SearchMatr(int[,] matrix, int r, int c)
+{
+    if (r>matrix.GetLength(0) || c> matrix.GetLength(1))
+{
+    Console.Write("Такой позиции в массиве нет");
+}
+else
+   Console.Write(matrix[r-1,c-1]);
+}
+
 Console.Clear();
 Console.Write("Введите позиции элемента в массиве: ");
 int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
@@ -22,11 +32,4 @@ int row = new Random().Next(2, 11);
 int column = new Random().Next(2, 11);
  int[,] matrix = new int[row, column];
 InputMatrix(matrix);
-
-if (size[0]>matrix.GetLength(0) || size[1]> matrix.GetLength(1))
-{
-    Console.Write("Такой позиции в массиве нет");
-}
-else
-   Console.Write(matrix[size[0]-1,size[1]-1]);
-
+SearchMatr(matrix, size[0], size[1]);
