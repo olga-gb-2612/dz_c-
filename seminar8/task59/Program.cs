f74561c5a00ef1136bf8fc272f6d7574145edcba
Olga <olga@podolyanchenko.ru>
2022-12-26 23:57:27 +0700

    задача 59

---
+++ seminar8/task59/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задайте двумерный массив из целых чисел. 
+//Напишите программу, которая удалит строку и столбец, на пересечении которых расположен наименьший элемент массива.
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11); // [1, 10]
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void SwapFirstLastString(int[,] matrix)
+{
+    int minValue = matrix[0, 0], minRow = 0, minColumn = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (minValue > matrix[i, j])
+            {
+                minValue = matrix[i, j];
+                minRow = i;
+                minColumn = j;
+            }
+        }
+    }
+    Console.WriteLine($"{minValue}({minRow}, {minColumn}) - минимум");   
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        if (i != minRow)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (j != minColumn)
+                    Console.Write($"{matrix[i, j]} \t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] coord = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[coord[0], coord[1]];
+Console.WriteLine("Начальный массив");
+InputMatrix(matrix);
+Console.WriteLine("Конечный массив");
+SwapFirstLastString(matrix);
