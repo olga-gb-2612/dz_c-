9980493366f8765bd04242ae333d79f6431f5cb4
Olga <olga@podolyanchenko.ru>
2022-12-28 00:55:45 +0700

    произведение матриц

---
+++ dz_seminar8/task58/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 11); // [1, 10]
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+int[,] MultiMatrix(int[,] matrix, int[,] matrix1)
+{
+    int[,] matrixC = new int[matrix.GetLength(0),matrix.GetLength(1)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrixC[i,j] = matrix[i,j]*matrix1[i,j];
+        }
+    }
+    return matrixC;
+}
+
+Console.Clear();
+Console.Write("Введите размерность матриц: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrixA = new int[size[0], size[1]];
+int[,] matrixB = new int[size[0], size[1]];
+Console.WriteLine("Матрица А:");
+InputMatrix(matrixA);
+PrintMatrix(matrixA);
+Console.WriteLine("Матрица B:");
+InputMatrix(matrixB);
+PrintMatrix(matrixB);
+Console.WriteLine("Произведение матриц А и В:");
+PrintMatrix(MultiMatrix(matrixA,matrixB));
