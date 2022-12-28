c58efc578c912ae873be3f65f174e081b6879337
Olga <olga@podolyanchenko.ru>
2022-12-28 19:40:59 +0700

    рек. сумма м и н

---
+++ dz_seminar9/task66/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+int rec(int m, int n)
+{
+    if (n == m)
+        return n;
+    return rec(m, n - 1) + n;
+}
+
+
+Console.Clear();
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(m, n));
